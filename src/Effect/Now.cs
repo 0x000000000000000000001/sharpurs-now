680157a533cfde4bd6bb29150fbd013803d9d037
0x1 <kevin.francart3@gmail.com>
2026-07-26 14:40:12 +0200

    feat: add C# FFI implementations

---
+++ src/Effect/Now.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Effect.Now;
+
+public static class FFI {
+    public static object Now() => throw new NotImplementedException("Not implemented: Now");
+    public static object GetTimezoneOffset() => throw new NotImplementedException("Not implemented: GetTimezoneOffset");
+}
